a368198f34b0e1b221274a35304863d9aaa6d9ea
mere3dith <meredith.summs@gmail.com>
2025-06-17 13:14:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -178,13 +178,13 @@ var values = [
 ]
 
 
-var shuffle = document.getElementById("shuffle1");
+var shuffle1 = document.getElementById("shuffle1");
 var game = document.getElementById("game");
 var verb = document.getElementById("verb");
 var challenge = document.getElementById("challenge");
 var value = document.getElementById("value");
 
-shuffle.addEventListener("click", function(){
+shuffle1.addEventListener("click", function(){
   var randomGame = Math.floor(Math.random() * games.length);
   var randomVerb = Math.floor(Math.random() * verbs.length);
   var randomChallenge = Math.floor(Math.random() * challenges.length);
